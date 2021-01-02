35157c4cf1e4b67bdff8636993a387e299ea0dbd
ShirinNagle <G00363458@gmit.ie>
2021-01-02 11:59:46 +0000

    working on getting basic webapp working, problems with Pylint in vscode

---
+++ windapp.py
@@ -0,0 +1,33 @@
+# flask for web app.
+import flask as fl
+# numpy for numerical work.
+import numpy as np
+# pandas for dataframes
+
+
+
+
+# jsonfy??
+
+#flask is allowing remote interaction with python script
+
+
+# Create a new web app. creating a new app in memory
+app = fl.Flask(__name__)
+
+#fl.Response()
+
+# Add root route.
+@app.route("/") # @app is a decorator
+def home():
+  return app.send_static_file('index.html')# sends a web page to home page
+
+# Add uniform route.
+@app.route('/api/uniform') # api returns json
+def uniform():
+  return {"value": np.random.uniform()}# return a dictionary
+
+# Add normal route.
+@app.route('/api/normal')
+def normal():
+  return {"value": np.random.normal()}# key value pair
